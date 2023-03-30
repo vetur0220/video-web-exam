f1eba0646f4b9fd85a96df92981936c544b0de36
Ash <vetur0220@gmail.com>
2023-03-30 22:17:23 +0800

    add vue-video-player js

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,34 @@
 <script setup>
-import { ref, reactive, onMounted } from "vue";
+import {
+  ref,
+  reactive,
+  onMounted,
+  onBeforeUnmount,
+  getCurrentInstance,
+} from "vue";
 import { VideoService } from "/src/api/api.ts";
 import axios from "axios";
 
+
+const playerOptions = {
+  controls: true,
+  autoplay: false,
+  sources: [
+    {
+      type: 'application/x-mpegURL',
+      src: 'http://localhost:3000/media/Audi_A4_S4.m3u8'
+    },
+    {
+      type: 'application/x-mpegURL',
+      src: 'http://localhost:3000/media/Bugatti_Chiron.m3u8'
+    },
+    {
+      type: 'application/x-mpegURL',
+      src: 'http://localhost:3000/media/Range_Rover_Sport_L322.m3u8'
+    }
+  ]
+};
+const instance = getCurrentInstance();
 
 const data = reactive({
   followingData: "",
@@ -12,7 +38,6 @@ const data = reactive({
 const getFollowing_list = async () => {
   const res = await VideoService.getFollowing_list();
   data.followingData = res.data;
-
 };
 const getFor_you_list = async () => {
   const res = await VideoService.getFor_you_list();
@@ -22,14 +47,21 @@ getFollowing_list();
 getFor_you_list();
 
 onMounted(() => {
-
+  
 });
+onBeforeUnmount(() => {});
+
+
+
 </script>
 
 <template>
-  <h1>test</h1>
-  {{data.followingData}}
-  {{data.forYouData}}
+ 
+  <!-- {{data.forYouData}} -->
+  <div id="shorts-container" class="shorts">
+    <video-player :options="playerOptions" />
+  </div>
+   {{ data.followingData }}
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import 'video.js/dist/video-js.css'
+import VideoPlayer from 'vue-video-player'
+createApp(App).use(VideoPlayer).mount('#app')
